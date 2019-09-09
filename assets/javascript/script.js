4fa34e428c480be97b99bb552bbd053558804f6f
Jonathan C <jsea@uw.edu>
2019-09-09 13:01:22 -0700

    Set up ajax call and created a function to perform it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,12 @@ var queryString = "hurricane"
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
 
-$.ajax({
-    url: queryURL,
-}).then(function(response){
-    console.log(response);
-})
+
+function grabNYTimesArticles(searchQuery, numberOfRecords, beginDate, endDate){
+
+    $.ajax({
+        url: queryURL,
+    }).then(function(response){
+        console.log(response);
+    })
+}
