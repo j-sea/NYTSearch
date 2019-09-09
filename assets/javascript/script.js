1f2853c60c311497cb763fa09dc18b6146cbebc0
Jonathan C <jsea@uw.edu>
2019-09-09 12:29:07 -0700

    Set up a dummy index.html we can overwrite with AndreNeha's later.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,3 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
+var queryString = "hurricane"
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
