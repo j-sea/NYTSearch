cbfe1eae1b3ce93f9a00e953973fb557a6cb2e35
Jonathan C <jsea@uw.edu>
2019-09-09 13:38:09 -0700

    added more logic for grabbing articles

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,32 @@
 
+
+
 function grabNYTimesArticles(searchQuery, numberOfRecords, beginDate, endDate){
     console.log('grabbing new york times articles');
-
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
-
+    
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "&begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
+    
+    var results;
+    
     $.ajax({
         url: queryURL,
-    }).then(function(response){
-        console.log(response);
+    }).then(function(data){
+        results = data.response.docs;
+
+        if (numberOfRecords <= 10) {
+            for (var i = numberOfRecords; i < results.length; i++) {
+                results.pop();
+            }
+
+            $('#search-results').text(JSON.stringify(results));
+        }
     });
+
+    return results;
 }
 
+$('#search-button').on('click', function(){
+    var searchQueryToUse = 'cat';
+
+    var records = grabNYTimesArticles(searchQueryToUse, 10, '20190101', '20190909');
+});
