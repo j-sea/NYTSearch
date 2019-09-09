160049d742ca8c5a50ea79165a28d7a34e3cfb45
Jonathan C <jsea@uw.edu>
2019-09-09 13:12:47 -0700

    added search query back into api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,13 @@
-var queryString = "hurricane"
-
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
-
 
 function grabNYTimesArticles(searchQuery, numberOfRecords, beginDate, endDate){
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
-    
-$.ajax({
+    console.log('grabbing new york times articles');
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchQuery + "begin_date=" + beginDate + "&end_date=" + endDate + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx";
+
+    $.ajax({
         url: queryURL,
     }).then(function(response){
         console.log(response);
-    })
+    });
 }
 
