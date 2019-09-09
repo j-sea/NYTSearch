689a285089d9b21b6b9c7104994e458f972e080a
Jonathan C <jsea@uw.edu>
2019-09-09 12:37:34 -0700

    Added simple ajax call to the NYTimes API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,9 @@
 var queryString = "hurricane"
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=v9ZuAGpf5SLtPeFaivEQF3WEYT4taWAx"
+
+$.ajax({
+    url: queryURL,
+}).then(function(response){
+    console.log(response);
+})
